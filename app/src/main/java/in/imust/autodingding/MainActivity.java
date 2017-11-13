f04b9847a1475e62f93745383382a1e0e642878c
declanzhang <declanzhang@gmail.com>
2017-11-13 14:01:10 +0800

    add notifications

--- app/src/main/java/in/imust/autodingding/MainActivity.java
+++ app/src/main/java/in/imust/autodingding/MainActivity.java
@@ -9,5 +9,4 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
--- app/src/main/java/in/imust/autodingding/NetworkConnectChangedReceiver.java
+++ app/src/main/java/in/imust/autodingding/NetworkConnectChangedReceiver.java
@@ -1,9 +1,13 @@
 package in.imust.autodingding;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
@@ -12,6 +16,8 @@ import android.os.Parcelable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NetworkConnectChangedReceiver extends BroadcastReceiver{
 
@@ -48,5 +54,62 @@ public class NetworkConnectChangedReceiver extends BroadcastReceiver{
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
+
+        // 立刻显示通知
+        showNotification(context, System.currentTimeMillis());
+
+
+        // 预备下班时显示通知
+        Date date = new Date();
+        date.setHours(18);
+        date.setMinutes(1);
+        showNotification(context, date.getTime());
     }
+
+    public void showNotification(Context context, long time) {
+        NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setContentInfo("GFW MUST DIE");
+        builder.setContentText("GFW MUST DIE");
+        builder.setContentTitle("打卡提醒");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setTicker("该打卡啦");
+        builder.setAutoCancel(true);
+
+        builder.setWhen(time);//设置时间，设置为系统当前的时间
+
+        Uri uri = Uri.parse("icebox://action/run_app/com.alibaba.android.rimet");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+
+//        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        Notification notification = builder.build();
+        /**
+         * vibrate属性是一个长整型的数组，用于设置手机静止和振动的时长，以毫秒为单位。
+         * 参数中下标为0的值表示手机静止的时长，下标为1的值表示手机振动的时长， 下标为2的值又表示手机静止的时长，以此类推。
+         */
+//        long[] vibrates = { 0, 1000, 1000, 1000 };
+//        notification.vibrate = vibrates;
+
+        /**
+         * 手机处于锁屏状态时， LED灯就会不停地闪烁， 提醒用户去查看手机,下面是绿色的灯光一 闪一闪的效果
+         */
+        notification.ledARGB = Color.GREEN;// 控制 LED 灯的颜色，一般有红绿蓝三种颜色可选
+        notification.ledOnMS = 500;// 指定 LED 灯亮起的时长，以毫秒为单位
+        notification.ledOffMS = 500;// 指定 LED 灯暗去的时长，也是以毫秒为单位
+        notification.flags = Notification.FLAG_SHOW_LIGHTS;// 指定通知的一些行为，其中就包括显示
+        // LED 灯这一选项
+
+//        Uri uri1 = Uri.parse("android.resource://" + getApplicationContext().getPackageName() + "/" + R.raw.mario);
+//        notification.sound = uri1;
+
+//        notification.defaults = Notification.DEFAULT_ALL;
+
+
+        manager.notify(1, notification);
+    }
+
 }
